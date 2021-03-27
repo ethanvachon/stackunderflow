e6be580faabf0ed01f63f53ba1bf6a1d92ad6a4c
ethanvachon <ethanvachon@gmail.com>
2021-03-26 21:14:45 -0600

    questions service and auth

--- Repositories/QuestionsRepository.cs
+++ Repositories/QuestionsRepository.cs
@@ -1,10 +1,32 @@
 using System;
+using System.Collections.Generic;
+using stackunderflow.Models;
 
 namespace stackunderflow.Repositories
 {
   public class QuestionsRepository
   {
-    internal object Get()
+    internal IEnumerable<Question> Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Question Get(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal int Create(Question newQuestion)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Question newQuestion)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id)
     {
       throw new NotImplementedException();
     }

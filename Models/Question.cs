44a4b6e3485dc12527a03a6e0567c8c2c5e12fa8
ethanvachon <ethanhvachon@gmail.com>
2021-03-31 16:33:12 -0600

    model fix

--- Models/Question.cs
+++ Models/Question.cs
@@ -7,11 +7,8 @@ namespace stackunderflow.Models
     [Required]
     public int Id { get; set; }
     public int Rating { get; set; }
-    [Required]
     public string Posted { get; set; }
-    [Required]
     public string Title { get; set; }
-    [Required]
     public string Body { get; set; }
     public string CreatorId { get; set; }
     public Profile Creator { get; set; }

5b6c759612e664bc0b9998105185ce8fc3cff77a
ethanvachon <ethanhvachon@gmail.com>
2021-04-08 17:07:01 -0600

    controller and service

--- Repositories/RatingsRepository.cs
+++ Repositories/RatingsRepository.cs
@@ -1,7 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using stackunderflow.Models;
+
 namespace stackunderflow.Repositories
 {
-    public class RatingsRepository
+  public class RatingsRepository
+  {
+    internal Rated Get(int ratingId)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal void Delete(int ratingId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal ActionResult<Rated> Create(Rated newRating)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }

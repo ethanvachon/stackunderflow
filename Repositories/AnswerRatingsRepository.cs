d318e5f51ea4e4df5ced64e08037b82834133e71
ethanvachon <ethanhvachon@gmail.com>
2021-05-15 18:53:24 -0600

    answer rating progress

---
+++ Repositories/AnswerRatingsRepository.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using stackunderflow.Models;
+
+namespace stackunderflow.Repositories
+{
+  public class AnswerRatingsRepository
+  {
+    internal int Create(AnswerRating newRating)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal IEnumerable<AnswerRating> GetByProfile(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal IEnumerable<AnswerRating> GetByAnswer(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

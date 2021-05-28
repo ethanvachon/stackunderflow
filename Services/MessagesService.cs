54dd77d262f31544e6285063b5204d0ab25feea2
ethanvachon <ethanhvachon@gmail.com>
2021-05-28 17:13:26 -0600

    chats actually working

--- Services/MessagesService.cs
+++ Services/MessagesService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using stackunderflow.Models;
 using stackunderflow.Repositories;
 
@@ -8,15 +9,21 @@ namespace stackunderflow.Services
   public class MessagesService
   {
     private readonly MessagesRepository _mrepo;
+    private readonly ChatsRepository _crepo;
 
-    public MessagesService(MessagesRepository mrepo)
+    public MessagesService(MessagesRepository mrepo, ChatsRepository crepo)
     {
       _mrepo = mrepo;
+      _crepo = crepo;
     }
 
     internal Message Create(Message newMessage)
     {
       newMessage.Id = _mrepo.Create(newMessage);
+      Chat chat = _crepo.GetOne(newMessage.ChatId);
+      Chat userTwo = _crepo.GetByProfile(chat.ParticipantTwo).FirstOrDefault(c => c.ParticipantTwo.Equals(newMessage.CreatorId));
+      newMessage.ChatId = userTwo.Id;
+      _mrepo.Create(newMessage);
       return newMessage;
     }
 
